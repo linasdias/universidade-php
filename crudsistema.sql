2ddeae167167594001629142ec4dff80bfbd8644
Ana <acsd@ufpi.edu.br>
2025-03-17 15:03:56 -0300

    banco de dados

--- crudsistema.sql
+++ crudsistema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 17/03/2025 às 19:00
+-- Tempo de geração: 17/03/2025 às 19:03
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
